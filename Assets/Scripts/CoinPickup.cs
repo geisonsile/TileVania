331e491aedf31e4992f3d4ea4f1bf68570b3acba
geisonsile <geisonsile@gmail.com>
2022-11-29 19:13:14 -0300

    Moeda animação e coletar adicionados

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
